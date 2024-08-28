17a682712fc3f8edd1eeacc96f5e6c8b2d47c983
Soumya975p <soumya384p@gmail.com>
2024-08-28 23:31:16 +0530

    Add files via upload

---
+++ date.java
@@ -0,0 +1,47 @@
+
+import java.util.*;
+public class date{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int month = sc.nextInt();
+        int day = sc.nextInt();
+        int year = sc.nextInt();
+        if(month == 1){
+            System.out.println("Birthday = " + day + " January, " + year);
+        }
+        if(month == 2){
+            System.out.println("Birthday = " + day + " February, " + year);
+        }
+        if(month == 3){
+            System.out.println("Birthday = " + day + " March, " + year);
+        }
+        if(month == 4){
+            System.out.println("Birthday = " + day + " April, " + year);
+        }
+        if(month == 5){
+            System.out.println("Birthday = " + day + " May, " + year);
+        }
+        if(month == 6){
+            System.out.println("Birthday = " + day + " June, " + year);
+        }
+        if(month == 7){
+            System.out.println("Birthday = " + day + " July, " + year);
+        }
+        if(month == 8){
+            System.out.println("Birthday = " + day + " August, " + year);
+        }
+        if(month == 9){
+            System.out.println("Birthday = " + day + " September, " + year);
+        }
+        if(month == 10){
+            System.out.println("Birthday = " + day + " October, " + year);
+        }
+        if(month == 11){
+            System.out.println("Birthday = " + day + " November, " + year);
+        }
+        if(month == 12){
+            System.out.println("Birthday = " + day + " December, " + year);
+        }
+        
+    }
+}
